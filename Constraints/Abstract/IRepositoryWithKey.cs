6e3b10bd9dfb7666b4f57757742ab3df8f8dba13
unknown <skmiecik@SKMIECIK-CHI-L.IWPrint.net>
2015-12-21 16:09:10 -0600

    update

---
+++ Constraints/Abstract/IRepositoryWithKey.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Constraints.Abstract
+{
+    public interface IRepositoryWithKey<TKey, T>
+    {
+        IEnumerable<T> GetItems();
+        T GetItem(TKey key);
+        void AddItem(TKey key, T item);
+    }
+}
